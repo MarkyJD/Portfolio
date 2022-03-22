f2eb60893f66df015d4f707aed486f550527532e
MarkyJD <54533821+MarkyJD@users.noreply.github.com>
2022-03-22 19:08:06 +1100

    Cleaned up 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import Background from '../components/Background';
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <Background />
+      <div className="z-50 fixed inset-0 text-3xl text-slate-200 font-serif flex justify-center space-x-5 items-center">
+        <p className="text-white">Sorry! This page does not exist</p>
+        <a
+          href="/"
+          className="text-xl px-5 py-2 text-orange-400 border rounded border-orange-400 hover:bg-orange-400 hover:text-slate-900 transition-all duration-300"
+        >
+          Return
+        </a>
+      </div>
+    </>
+  );
 }
